3144b2cf6e11ea8b5564238167b55e6ec1697db2
Jason Conner <jason.r.conner@gmail.com>
2016-06-15 18:35:59 -0500

    Updated the mixin so the static  method can be called directly on  instead of . Updated the documentation in open_string to reflect the change from  to  and to include some details about the use of the sub-module for static methods. Updated the tests to make them compatible with this refactor.

--- lib/open_string.rb
+++ lib/open_string.rb
@@ -11,34 +11,45 @@ module OpenString
     self == reverse
   end
 
-  # Public: Generates a randomized string of a length specified by +length+.
+  # Methods meant to be members of the String class need to go in here. The
+  # machinery here is a little odd: since the +randomize+ declaration isn't
+  # prefaced with a +self+, it doesn't look static. Once +String+ is set to
+  # +extend OpenInteger::ClassMethods+, this is treated like a static String
+  # method.
   #
-  # +length+::    The number of characters to include in the string
-  # +*charsets+:: Arrays of character sets to use in generating the random
-  #   string. This can take array literals, variables, or constants. Some
-  #   character sets are predefined in test/support/character_sets.rb
-  #
-  # ---
-  # :section: Examples
-  # # Generates a 10-character string using only lowercase letters
-  # str = random_string_of_length(10, CharacterSets.lowercase)
-  #
-  # # Generates a 5-character string using only ['a', 's', 'd', '1', '2', '3']
-  # str = random_string_of_length(5, %w(a s d 1 2 3))
-  # ---
-  def self.randomize(length, *charsets)
-    raise(ArgumentError,
-          'Do not pass nil for length.') if length.nil?
-    raise(ArgumentError,
-          'Length must be a non-zero positive integer.') if length < 1
-    raise(ArgumentError,
-          'Specify a character set.') if charsets.empty?
+  # If this inner module wasn't created and, instead, String was set to
+  # +include OpenString+, consumers would need to call static methods via
+  # +OpenString+ like so: +OpenString.randomize(10, CharacterSets.all)+.
+  module ClassMethods
+    # Public: Generates a randomized string of a length specified by +length+.
+    #
+    # +length+::    The number of characters to include in the string
+    # +*charsets+:: Arrays of character sets to use in generating the random
+    #   string. This can take array literals, variables, or constants. Some
+    #   character sets are predefined in test/support/character_sets.rb
+    #
+    # ---
+    # :section: Examples
+    # # Generates a 10-character string using only lowercase letters
+    # str = String.randomize(10, CharacterSets.lowercase)
+    #
+    # # Generates a 5-character string using only ['a', 's', 'd', '1', '2', '3']
+    # str = String.randomize(5, %w(a s d 1 2 3))
+    # ---
+    def randomize(length, *charsets)
+      raise(ArgumentError,
+            'Do not pass nil for length.') if length.nil?
+      raise(ArgumentError,
+            'Length must be a non-zero positive integer.') if length < 1
+      raise(ArgumentError,
+            'Specify a character set.') if charsets.empty?
 
-    Array.new(length) { charsets.flatten.sample }.join
+      Array.new(length) { charsets.flatten.sample }.join
+    end
   end
 end
 
 # Public: Mixing OpenString extensions into String.
 class String
-  include OpenString
+  extend OpenString::ClassMethods
 end
--- test/specs/randomize.rb
+++ test/specs/randomize.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 require File.expand_path '../../helpers/helper', __FILE__
+include OpenString
 
 rand_size = 1 + rand(1_000)
 
-all_string         = OpenString.randomize(rand_size, CharacterSets.all)
+all_string         = String.randomize(rand_size, CharacterSets.all)
 custom_characters  = %w(3 x A M p 1 e C h @ r $)
-custom_string      = OpenString.randomize(rand_size, custom_characters)
-lowercase_string   = OpenString.randomize(rand_size, CharacterSets.lowercase)
-number_string      = OpenString.randomize(rand_size, CharacterSets.numbers)
-special_string     = OpenString.randomize(rand_size, CharacterSets.specials)
-upper_lower_string = OpenString.randomize(rand_size,
-                                          CharacterSets.uppercase,
-                                          CharacterSets.lowercase)
-uppercase_string   = OpenString.randomize(rand_size, CharacterSets.uppercase)
+custom_string      = String.randomize(rand_size, custom_characters)
+lowercase_string   = String.randomize(rand_size, CharacterSets.lowercase)
+number_string      = String.randomize(rand_size, CharacterSets.numbers)
+special_string     = String.randomize(rand_size, CharacterSets.specials)
+upper_lower_string = String.randomize(rand_size,
+                                      CharacterSets.uppercase,
+                                      CharacterSets.lowercase)
+uppercase_string   = String.randomize(rand_size, CharacterSets.uppercase)
 
 describe 'all character sets' do
   it 'may generate strings with uppercase letters' do
@@ -44,6 +45,93 @@ describe 'custom character sets' do
 
   it 'cannot generate strings with any other characters' do
     refute_match(/[^3xAMp1eCh@r$]+/, custom_string)
+  end
+end
+
+describe 'general randomize behavior' do
+  it 'returns a string of the correct length' do
+    (1..1_000).each do |n|
+      str = String.randomize(n, CharacterSets.lowercase)
+      assert_equal n, str.length
+    end
+  end
+
+  it 'raises ArgumentError when receiving no character set' do
+    assert_raises ArgumentError do
+      String.randomize(10)
+    end
+  end
+
+  it 'raises ArgumentError when receiving a nil length' do
+    assert_raises ArgumentError do
+      String.randomize(nil, CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving an empty string length' do
+    assert_raises ArgumentError do
+      String.randomize('', CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving all white space for length' do
+    assert_raises ArgumentError do
+      String.randomize('     ', CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving no arguments' do
+    assert_raises ArgumentError do
+      String.randomize
+    end
+  end
+
+  it 'raises ArgumentError when receiving a numerical string length' do
+    assert_raises ArgumentError do
+      String.randomize('10', CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving mixed letters and numbers length' do
+    assert_raises ArgumentError do
+      String.randomize('abc123', CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving all letters for length' do
+    assert_raises ArgumentError do
+      String.randomize('abc', CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving mixed special and letter length' do
+    assert_raises ArgumentError do
+      String.randomize('!@#abc', CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving mixed special and number length' do
+    assert_raises ArgumentError do
+      String.randomize('!@#123', CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving all special characters length' do
+    assert_raises ArgumentError do
+      String.randomize('!@#', CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving a length of 0' do
+    assert_raises ArgumentError do
+      String.randomize(0, CharacterSets.lowercase)
+    end
+  end
+
+  it 'raises ArgumentError when receiving a negative number for length' do
+    assert_raises ArgumentError do
+      String.randomize(0 - rand(1_000), CharacterSets.lowercase)
+    end
   end
 end
 
@@ -154,92 +242,5 @@ describe 'uppercase character set' do
 
   it 'cannot generate strings with underscores' do
     refute_match(/_/, uppercase_string)
-  end
-end
-
-describe 'general randomize behavior' do
-  it 'returns a string of the correct length' do
-    (1..1_000).each do |n|
-      str = OpenString.randomize(n, CharacterSets.lowercase)
-      assert_equal n, str.length
-    end
-  end
-
-  it 'raises ArgumentError when receiving no character set' do
-    assert_raises ArgumentError do
-      OpenString.randomize(10)
-    end
-  end
-
-  it 'raises ArgumentError when receiving a nil length' do
-    assert_raises ArgumentError do
-      OpenString.randomize(nil, CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving an empty string length' do
-    assert_raises ArgumentError do
-      OpenString.randomize('', CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving all white space for length' do
-    assert_raises ArgumentError do
-      OpenString.randomize('     ', CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving no arguments' do
-    assert_raises ArgumentError do
-      OpenString.randomize
-    end
-  end
-
-  it 'raises ArgumentError when receiving a numerical string length' do
-    assert_raises ArgumentError do
-      OpenString.randomize('10', CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving mixed letters and numbers length' do
-    assert_raises ArgumentError do
-      OpenString.randomize('abc123', CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving all letters for length' do
-    assert_raises ArgumentError do
-      OpenString.randomize('abc', CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving mixed special and letter length' do
-    assert_raises ArgumentError do
-      OpenString.randomize('!@#abc', CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving mixed special and number length' do
-    assert_raises ArgumentError do
-      OpenString.randomize('!@#123', CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving all special characters length' do
-    assert_raises ArgumentError do
-      OpenString.randomize('!@#', CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving a length of 0' do
-    assert_raises ArgumentError do
-      OpenString.randomize(0, CharacterSets.lowercase)
-    end
-  end
-
-  it 'raises ArgumentError when receiving a negative number for length' do
-    assert_raises ArgumentError do
-      OpenString.randomize(0 - rand(1_000), CharacterSets.lowercase)
-    end
   end
 end
